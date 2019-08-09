19776f24415e8e083924cc3444852998098a6701
Steven A. Rodriguez <hi@stevenarodriguez.com>
2019-08-09 18:15:16 -0500

    changed hack program date

--- js/script.js
+++ js/script.js
@@ -90,7 +90,7 @@
 
     // Count down setup
 
-    $('.countdown').countdown('2019/8/17', function(event) {
+    $('.countdown').countdown('2019/8/20', function(event) {
         $(this).html(event.strftime('%D days %H:%M:%S'));
     });
 
